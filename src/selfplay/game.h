6b676126374c1570fed062bc89eec0261ca34e70
Dietrich Kappe <dkappe@gmail.com>
2018-12-17 20:06:52 -0600

    Added self-play goodies.

--- src/selfplay/game.h
+++ src/selfplay/game.h
@@ -103,6 +103,12 @@ class SelfPlayGame {
 
   // Training data to send.
   std::vector<V3TrainingData> training_data_;
+
+
+    // Syzygy
+    SyzygyTablebase *syzygy_tb_;
+
+    static SyzygyTablebase* GetTB(std::string path);
 };
 
 }  // namespace lczero
