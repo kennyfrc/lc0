0d52ff9efe5b7408a1c3f87bfd82dd5a31a28791
Daniel Uranga <daniel.uranga@gmail.com>
2019-12-15 00:28:02 -0300

    Update default tune

--- src/mcts/params.cc
+++ src/mcts/params.cc
@@ -196,8 +196,8 @@ void SearchParams::Populate(OptionsParser* options) {
   options->Add<IntOption>(kMiniBatchSizeId, 1, 1024) = 256;
   options->Add<IntOption>(kMaxPrefetchBatchId, 0, 1024) = 32;
   options->Add<BoolOption>(kLogitQId) = false;
-  options->Add<FloatOption>(kCpuctId, 0.0f, 100.0f) = 3.54f;
-  options->Add<FloatOption>(kCpuctBaseId, 1.0f, 1000000000.0f) = 19652.0f;
+  options->Add<FloatOption>(kCpuctId, 0.0f, 100.0f) = 2.90f;
+  options->Add<FloatOption>(kCpuctBaseId, 1.0f, 1000000000.0f) = 53500.0f;
   options->Add<FloatOption>(kCpuctFactorId, 0.0f, 1000.0f) = 2.74f;
   options->Add<FloatOption>(kTemperatureId, 0.0f, 100.0f) = 0.0f;
   options->Add<IntOption>(kTempDecayMovesId, 0, 100) = 0;
@@ -212,13 +212,13 @@ void SearchParams::Populate(OptionsParser* options) {
   options->Add<BoolOption>(kVerboseStatsId) = false;
   options->Add<BoolOption>(kLogLiveStatsId) = false;
   std::vector<std::string> fpu_strategy = {"reduction", "absolute"};
-  options->Add<ChoiceOption>(kFpuStrategyId, fpu_strategy) = "absolute";
+  options->Add<ChoiceOption>(kFpuStrategyId, fpu_strategy) = "reduction";
   options->Add<FloatOption>(kFpuValueId, -100.0f, 100.0f) = -1.0f;
   fpu_strategy.push_back("same");
   options->Add<ChoiceOption>(kFpuStrategyAtRootId, fpu_strategy) = "same";
-  options->Add<FloatOption>(kFpuValueAtRootId, -100.0f, 100.0f) = 1.2f;
+  options->Add<FloatOption>(kFpuValueAtRootId, -100.0f, 100.0f) = 0.7;
   options->Add<IntOption>(kCacheHistoryLengthId, 0, 7) = 0;
-  options->Add<FloatOption>(kPolicySoftmaxTempId, 0.1f, 10.0f) = 1.87f;
+  options->Add<FloatOption>(kPolicySoftmaxTempId, 0.1f, 10.0f) = 1.8f;
   options->Add<IntOption>(kMaxCollisionEventsId, 1, 1024) = 32;
   options->Add<IntOption>(kMaxCollisionVisitsId, 1, 1000000) = 9999;
   options->Add<BoolOption>(kOutOfOrderEvalId) = true;
