440b78ed1a129af409561263080085e4fc48e880
Daniel Uranga <daniel.uranga@gmail.com>
2019-12-14 22:29:24 -0300

    Update default tune

--- src/mcts/params.cc
+++ src/mcts/params.cc
@@ -193,12 +193,12 @@ const OptionId SearchParams::kShortSightednessId{
 void SearchParams::Populate(OptionsParser* options) {
   // Here the uci optimized defaults" are set.
   // Many of them are overridden with training specific values in tournament.cc.
-  options->Add<IntOption>(kMiniBatchSizeId, 1, 1024) = 256;
-  options->Add<IntOption>(kMaxPrefetchBatchId, 0, 1024) = 32;
+  options->Add<IntOption>(kMiniBatchSizeId, 1, 1024) = 4;
+  options->Add<IntOption>(kMaxPrefetchBatchId, 0, 1024) = 1;
   options->Add<BoolOption>(kLogitQId) = false;
   options->Add<FloatOption>(kCpuctId, 0.0f, 100.0f) = 2.90f;
-  options->Add<FloatOption>(kCpuctBaseId, 1.0f, 1000000000.0f) = 53500.0f;
-  options->Add<FloatOption>(kCpuctFactorId, 0.0f, 1000.0f) = 2.74f;
+  options->Add<FloatOption>(kCpuctBaseId, 1.0f, 1000000000.0f) = 19500.0f;
+  options->Add<FloatOption>(kCpuctFactorId, 0.0f, 1000.0f) = 2.0f;
   options->Add<FloatOption>(kTemperatureId, 0.0f, 100.0f) = 0.0f;
   options->Add<IntOption>(kTempDecayMovesId, 0, 100) = 0;
   options->Add<IntOption>(kTemperatureCutoffMoveId, 0, 1000) = 0;
@@ -213,10 +213,10 @@ void SearchParams::Populate(OptionsParser* options) {
   options->Add<BoolOption>(kLogLiveStatsId) = false;
   std::vector<std::string> fpu_strategy = {"reduction", "absolute"};
   options->Add<ChoiceOption>(kFpuStrategyId, fpu_strategy) = "reduction";
-  options->Add<FloatOption>(kFpuValueId, -100.0f, 100.0f) = -1.0f;
+  options->Add<FloatOption>(kFpuValueId, -100.0f, 100.0f) = 0.7;
   fpu_strategy.push_back("same");
   options->Add<ChoiceOption>(kFpuStrategyAtRootId, fpu_strategy) = "same";
-  options->Add<FloatOption>(kFpuValueAtRootId, -100.0f, 100.0f) = 0.7;
+  options->Add<FloatOption>(kFpuValueAtRootId, -100.0f, 100.0f) = 1.2f;
   options->Add<IntOption>(kCacheHistoryLengthId, 0, 7) = 0;
   options->Add<FloatOption>(kPolicySoftmaxTempId, 0.1f, 10.0f) = 1.8f;
   options->Add<IntOption>(kMaxCollisionEventsId, 1, 1024) = 32;
